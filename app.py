be34a73e52c7678defa8e024c410c79a564d5ff9
vsem <virtualik@gmail.com>
2022-09-26 19:01:16 +0300

    POST function extended

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import requests
 
-from flask import Flask, request, jsonify, make_response
-from flask_restful import Resource, Api
+from flask import Flask, request, jsonify
+from flask_restful import Api
 
 import config
 
@@ -30,12 +30,17 @@ def get_repos(username):
             }
         ), 200
 
+    return jsonify(
+        {
+            "result": "unknown error"
+        }
+    )
 
-@app.route("/<username>/<repo_name>/issue")
+@app.route("/<username>/<repo_name>/issue", methods=["POST"])
 def post_issue(username, repo_name):
     if request.method == ["POST"]:
         r = requests.post(
-            url=f"{config.URL}/{username}/{repo_name}/issues",
+            url=f"{config.URL}/repos/{username}/{repo_name}/issues",
             json=(
                 {
                     "title": "test issue title",
@@ -43,12 +48,19 @@ def post_issue(username, repo_name):
                 }
             )
         )
-
+        print("\nr.status_code = ", r.status_code)
         return jsonify(
             {
                 "issue created": True
             }
-        )
+        ), 201
+
+    return jsonify(
+        {
+            "issue created": False,
+            "reason": "unknown error"
+        }
+    )
 
 
 if __name__ == "__main__":
